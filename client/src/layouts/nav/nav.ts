e9d778c4f2e810dd7d7548617b82ee2d5b25cf95
SalikBackEnd <salikahmed301@gmail.com>
2026-01-06 15:34:37 +0500

    Added routerlink and routerlinkactive modules . Also configered navigation routes in navbar.

--- client/src/layouts/nav/nav.ts
+++ client/src/layouts/nav/nav.ts
@@ -2,15 +2,17 @@ import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { LoginDto } from '../../types/user';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule,RouterLink,RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   protected accountService = inject(AccountService);
+  private router = inject(Router);
   protected cred:LoginDto = {email:'',password:''};
 
   login(){
@@ -18,6 +20,7 @@ export class Nav {
       next: (response) => {
         this.accountService.currentUser.set(response);
         this.cred = {email:'',password:''};
+        this.router.navigateByUrl('/members');
       },
       error: err => alert(err.message),
       complete: ()=> console.log('Completed the login request')
@@ -25,5 +28,7 @@ export class Nav {
   }
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
+
   }
 }

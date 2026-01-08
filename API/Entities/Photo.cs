ac4a3f27bdd4061e4b052678a797da870600573c
SalikBackEnd <salikahmed301@gmail.com>
2026-01-08 14:25:59 +0500

    Added two new entities to the application members and photo.

---
+++ API/Entities/Photo.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace API.Entities;
+
+public class Photo
+{
+    public int Id {get;set;}
+    public required string Url {get;set;}
+    public string? PublicId {get;set;}
+
+    //Navigation Property
+    public Member Member{get;set;} = null!;
+}

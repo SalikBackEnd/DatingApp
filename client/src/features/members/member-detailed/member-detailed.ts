d4e462e485c0cc007b629e45d33bc94df379b652
SalikBackEnd <salikahmed301@gmail.com>
2026-01-11 23:43:41 +0500

    Added an Edit Button on profile component. TO enable /disable edit mode.

--- client/src/features/members/member-detailed/member-detailed.ts
+++ client/src/features/members/member-detailed/member-detailed.ts
@@ -1,9 +1,10 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { filter } from 'rxjs';
 import { Member } from '../../../types/member';
 import { AgePipe } from '../../../core/pipes/age-pipe';
+import { AccountService } from '../../../core/services/account-service';
 
 @Component({
   selector: 'app-member-detailed',
@@ -12,11 +13,17 @@ import { AgePipe } from '../../../core/pipes/age-pipe';
   styleUrl: './member-detailed.css',
 })
 export class MemberDetailed implements OnInit{
-  private memberService = inject(MemberService);
+  protected memberService = inject(MemberService);
+  private accountService = inject(AccountService);
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   protected member = signal<Member | undefined>(undefined);
   protected title = signal<String | undefined>('Profile');
+  protected isLoggedInUser = computed(()=>{
+    console.log(this.accountService.currentUser()?.id);
+    console.log(this.route.snapshot.paramMap.get('id'))
+    return this.accountService.currentUser()?.id === this.route.snapshot.paramMap.get('id');
+  })
 
   ngOnInit(): void {
     this.route.data.subscribe({
